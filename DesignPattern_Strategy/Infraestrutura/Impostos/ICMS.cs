3a363b45faa6ff6d4bcb6096dbca2331a12c79b4
MarcoADeCSilva <marcoacsilva@live.com>
2021-11-17 22:22:23 -0300

    Inclusao do imposto ICCC

--- DesignPattern_Strategy/Infraestrutura/Impostos/ICMS.cs
+++ DesignPattern_Strategy/Infraestrutura/Impostos/ICMS.cs
@@ -7,7 +7,7 @@ namespace DesignPattern_Strategy.Infraestrutura.Impostos
     {
         public double Calcula(Orcamento orcamento)
         {
-            return orcamento.Valor * 0.1;
+            return ((orcamento.Valor * 0.05) + 50.00);
         }
     }
 }
--- DesignPattern_Strategy/Program.cs
+++ DesignPattern_Strategy/Program.cs
@@ -12,12 +12,14 @@ namespace DesignPattern_Strategy
         {
             IImposto iss = new ISS();
             IImposto icms = new ICMS();
+            IImposto iccc = new ICCC();
 
             Orcamento orcamento = new Orcamento(500.0);
             CalculadorDeImpostos calculador = new CalculadorDeImpostos();
 
             calculador.RealizaCalculo(orcamento, iss);
             calculador.RealizaCalculo(orcamento, icms);
+            calculador.RealizaCalculo(orcamento, iccc);
 
             Console.ReadKey();
         }
